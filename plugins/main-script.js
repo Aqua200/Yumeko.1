e447901d7907bfeb8dcfb26c6557f7e954816c59
Aqua200 <chinquepapa@gmail.com>
2024-10-14 19:20:54 -0700

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,17 +2,17 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-let res = await fetch('https://api.github.com/repos/elias1524/Veggeto-Bot-MD')
+let res = await fetch('https://github.com/Aqua200/Yumeko.1/tree/main')
 let json = await res.json()
 try {
 let txt = `*乂  S C R I P T  -  M A I N  乂*\n\n`
-    txt += `✩  *Nombre* : ${json.name}\n`
-    txt += `✩  *Visitas* : ${json.watchers_count}\n`
-    txt += `✩  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
-    txt += `✩  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-    txt += `✩  *Url* : ${json.html_url}\n`
-    txt += `✩  *Forks* : ${json.forks_count}\n`
-    txt += `✩  *Stars* : ${json.stargazers_count}\n\n`
+    txt += `✩  *Nombre🌸* : ${json.name}\n`
+    txt += `✩  *Visitas🌸* : ${json.watchers_count}\n`
+    txt += `✩  *Peso🌸* : ${(json.size / 1024).toFixed(2)} MB\n`
+    txt += `✩  *Actualizada🍓* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
+    txt += `✩  *Url❤️* : ${json.html_url}\n`
+    txt += `✩  *Forks🌹* : ${json.forks_count}\n`
+    txt += `✩  *Stars⭐* : ${json.stargazers_count}\n\n`
     txt += `> *${dev}*`
 
 await conn.sendMessage(m.chat, {text: txt, contextInfo: { forwardingScore: 999, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterName: '𝒀𝒖𝒎𝒆𝒌𝒐𝑩𝒐𝒕-𝑫𝒎🌸', newsletterJid: "120363276986902836@newsletter", }, externalAdReply: { title: packname, body: dev, thumbnailUrl: 'https://qu.ax/DhraC.jpg', sourceUrl: yt, mediaType: 1, renderLargerThumbnail: true }}}, {quoted: fkontak})
